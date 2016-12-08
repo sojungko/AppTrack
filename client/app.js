293cca261cd14ef9d6fd633630c7c6dee77a5f41
JW Garrison <jwolfgarrison@gmail.com>
2016-12-08 16:52:40 -0500

    fix(App.js): Fix an issue where some modules are not being loaded
    Add at.auth, fix typo AddAppsController

--- client/app.js
+++ client/app.js
@@ -1,45 +1,51 @@
-angular.module('at', ['at.services','ngRoute'])
-.config(function($routeProvider, $httpProvider) {
+angular.module('at', [
+  'at.addApps',
+  'at.auth',
+  'at.services',
+  'ngRoute',
+])
+
+.config(($routeProvider, $httpProvider) => {
   $routeProvider
   .when('/', {
     templateUrl: './app/allApps/allApps.html',
     controller: 'allAppsController',
-    authenticate: false //change to true once auth works
+    authenticate: false, //change to true once auth works
   })
   .when('/addApps', {
     templateUrl: './app/addApps/addApps.html',
-    controller: 'addAppsController',
-    authenticate: false //change to true once auth works
+    controller: 'AddAppsController',
+    authenticate: false, //change to true once auth works
   })
   .when('/login', {
     templateUrl: './login/login.html',
-    controller: 'AuthController'
+    controller: 'AuthController',
   })
   .when('/signup', {
     templateUrl: './login/signup.html',
-    controller: 'AuthController'
+    controller: 'AuthController',
   })
-  .otherwise({
-    redirectTo: '/'
-  });
+  .otherwise({ redirectTo: '/' });
   $httpProvider.interceptors.push('AttachTokens'); // will attach token to the localStorage
 })
 
-.factory('AttachTokens', function ($window) {
-  var attach = {
-    request: function (object) {
-      var jwt = $window.localStorage.getItem('app-track');
+.factory('AttachTokens', ($window) => {
+  const attach = {
+    request(object) {
+      const jwt = $window.localStorage.getItem('app-track');
       if (jwt) {
         object.headers['x-access-token'] = jwt;
       }
+
       object.headers['Allow-Control-Allow-Origin'] = '*';
       return object;
-    }
+    },
   };
   return attach;
 })
-.run(function ($rootScope, $location, Auth) { // handles the authentication where authentication: true above
-  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+
+.run(($rootScope, $location, Auth) => { // handles the authentication where authentication: true above
+  $rootScope.$on('$routeChangeStart', (evt, next, current) => {
     if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
       $location.path('/login');
     }
