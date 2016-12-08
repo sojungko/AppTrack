928124f1ce86dc882a7850c67ed424f59ad281d8
Adam Wang <adamw3494@gmail.com>
2016-12-08 14:44:15 -0500

    refactor: update backend files
    update pack.json, user controller and model, position controller and server.js

--- server/positions/positionController.js
+++ server/positions/positionController.js
@@ -1,40 +1,32 @@
 const Q = require('q');
 const Position = require('./positionModel.js');
-// this file is not used
+// this file is not used -- im not sure what that means??
 
 const findPosition = Q.nbind(Position.findOne, Position);
 const createPosition = Q.nbind(Position.create, Position);
 const findAllPositions = Q.nbind(Position.find, Position);
 
 module.exports = {
-
-	// testInsert: () {
-	// 	// Position.insert
-	// }
-
 	allPositions: (req, res) => {
-
 		findAllPositions({})
 			.then((positions) => {
 				res.json(positions);
 			})
 			.fail((err) => {
-				console.log(err)
+				console.log("ERROR in retreiving all positions, server/positions/positionController : ", err);
 			});
 	},
 
 	newPosition: (req, res) => {
-
 		createPosition(req.body.data)
-
 		  .then((createdPosition) => {
 		  	if (createdPosition) {
-		  		console.log("request", req.body.data);
+		  		console.log("CREATE position request: ", req.body.data);
 		  		res.json(createdPosition);
 		  	}
 		  })
-		  .fail((error) => {
-		  	console.log(error);
+		  .fail((err) => {
+		  	console.log("ERROR in creating position, server/positions/positionController : ", err);
 		  });
 	}
 
--- server/users/userModel.js
+++ server/users/userModel.js
@@ -1,12 +1,30 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
 // this file describes the userSchema for how mongoose writes to the database
 // this is only used in the sign up endpoint
-
-
 
 var UserSchema = new mongoose.Schema({
   username: { type: String, required: true, index: { unique: true } },
   password: { type: String, required: true }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+var User = mongoose.model('User', UserSchema);
+
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) { return cb(err); }
+    cb(null, isMatch);
+  });
+};
+
+UserSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+module.exports = User;
