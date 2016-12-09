bb24131cba5beee5474bd8ce70937407146649b2
Jerry Shanahan <JerryShanahan@Jerrys-MacBook-Pro.local>
2016-12-09 17:14:51 -0500

    feat(adding email templates):

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const positionController = require('./positions/positionController.js');
 const userController = require('./users/userController.js');
+const email = require('./email/emailModel.js');
 
 //post request endpoint that is initialized in our $http post request in our Angular formController.
 // This saves all form inputs to our database which can be viewed via https://mlab.com/
@@ -16,5 +17,11 @@ router.get('/api/form', positionController.allPositions);
 router.post('/api/form/:id', positionController.addStage);
 
 router.post('/api/edit/:id', positionController.editStage);
+
+router.post('/api/newAppEmail', email.newSend);
+
+router.post('/api/closedAppEmail', email.closedSend);
+
+
 
 module.exports = router;
