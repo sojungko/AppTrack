2720a7da40c035514bd657f9621f9181066eec71
Adam Wang <adamw3494@gmail.com>
2016-12-09 11:19:20 -0500

    fix(usermodel): fixed user model saving to db
    fixed user model saving to db, and signup with correct res.send token

--- server/users/userController.js
+++ server/users/userController.js
@@ -1,25 +1,31 @@
 const User = require('./userModel.js');
 const jwt = require('jwt-simple');
+var bcrypt = require('bcrypt-nodejs');
+
 
 module.exports = {
 	signUp: (req, res) => {
 	  var username = req.body.username;
 	  var password = req.body.password;
+		var email = req.body.email;
 
 	  User.findOne({ username: username })
 	    .exec((err, user) => {
 	      if (!user) {
-	        var newUser = new User({
-	          username: username,
-	          password: password
-	        });
-	        newUser.save(function(err, newUser) {
-	          if (err) {
-	            res.status(500).send(err);
-	          }
-						var token = jwt.encode(username, 'apptrak')
-	          res.send(token);
-	        });
+					bcrypt.hash(password, null, null, (err, hash) => {
+						var newUser = new User({
+							username: username,
+							password: hash,
+							email: email
+						});
+						newUser.save(function(err, newUser) {
+							if (err) {
+								res.status(500).send(err);
+							}
+							var token = jwt.encode(newUser, 'apptrak')
+							res.send(newUser);
+						});
+					})
 	      } else {
 	        console.log('Account already exists');
 	        res.send(401);
@@ -33,12 +39,13 @@ module.exports = {
 
 	  User.findOne({ username: username })
 	    .exec(function(err, user) {
+				var user = user;
 	      if (!user) {
-	        res.redirect('/login'); //is this redirect going to work?
+	        res.send(401);
 	      } else {
 	        User.comparePassword(password, user.password, function(err, match) {
 	          if (match) {
-	            var token = jwt.encode(username, 'apptrak');
+	            var token = jwt.encode(user, 'apptrak');
 							res.send(token);
 	          } else {
 	            res.send(401);
