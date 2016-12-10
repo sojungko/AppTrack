b3f05908735b780e2846638a94cb11ffb9397e7c
Jerry Shanahan <JerryShanahan@Jerrys-MacBook-Pro.local>
2016-12-10 12:22:18 -0500

    feat(emails): weekly reminder email in place

--- server/email/emailHelper.js
+++ server/email/emailHelper.js
@@ -9,21 +9,23 @@ var emails = require('./emailModel.js');
 // });
 var weeklyReminderSender = function() {
   var reg = /\w+/;
+  var user;
   setInterval(function(){
     Users.find({username:reg}, function(err, data) {
       var users = data;
       console.log('USERS !!!!!!!: ', users)
       for(var i = 0; i < users.length; i++) {
-        Apps.find({userId:users[i].id}, function(err, apps) {
-          console.log('APPLICATIONS: ', users[i])
+        user = users[i]
+        Apps.find({userId:user.id}, function(err, apps) {
+          console.log('APPLICATIONS: ', user)
           var userApps = apps;
           if(userApps.length > 0) {
-            emails.send(users[i], userApps);
+            emails.send(user, userApps);
           }
         });
       }
     });
-  }, 1000 * 60 /* * 60 * 24 */)
+  }, 1000 * 5 /* * 60 * 24 */)
 };
 // var allApps = Apps.find({});
 //   console.log('WEEKLY REMINDER !!')
