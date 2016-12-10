15a905b79b62cf1ce9dbb089ceeddfce6f1b9114
JW Garrison <jwolfgarrison@gmail.com>
2016-12-10 12:14:17 -0500

    feat(SignUp): Signup page input validation

--- client/app/login/auth.js
+++ client/app/login/auth.js
@@ -19,7 +19,6 @@ angular.module('at.auth', [])
   $scope.signup = () => {
     //pass entire user scope down
     //receives jwt encoded username after backend query to add user resolves
-    console.log('SCOPE.USER : ', $scope.user);
     Auth.signup($scope.user)
       .then((token) => {
         $window.localStorage.setItem('app-trak', token);
