2b12ace096c7adb2e117b20f774f1f18acbe03ae
Jerry Shanahan <JerryShanahan@Jerrys-MacBook-Pro.local>
2016-12-10 12:18:52 -0500

    feat(emails): test weekly reminder emails

--- server/email/emailHelper.js
+++ server/email/emailHelper.js
@@ -2,34 +2,57 @@ var Apps = require('../applications/applicationModel.js');
 var Users = require('../users/userModel.js');
 var emails = require('./emailModel.js');
 
-var allUsers = Users.find();
-var allApps = Apps.find();
+
+//  = Apps.find({}).toArray(function(err, results) {
+//   if(err) {console.log('ERROR: ', err)}
+//   return
+// });
 var weeklyReminderSender = function() {
-  console.log('WEEKLY REMINDER !!')
-  setInterval(function() {
-    console.log('allUsers helpers.js', allUsers);
-    console.log('allApps helpers.js', allApps);
-    var day = new Date();
-    day = day.getDay(); // return number 0-6 where 0 is sunday
-    if(day === 1) {
-      for(var i = 0; i < allUsers.length; i++) {
-        var userApps = [];
-        allApps.forEach(function(app) {
-          if(app.userId === allUsers[i].id) {
-            userApps.push(app);
+  var reg = /\w+/;
+  setInterval(function(){
+    Users.find({username:reg}, function(err, data) {
+      var users = data;
+      console.log('USERS !!!!!!!: ', users)
+      for(var i = 0; i < users.length; i++) {
+        Apps.find({userId:users[i].id}, function(err, apps) {
+          console.log('APPLICATIONS: ', users[i])
+          var userApps = apps;
+          if(userApps.length > 0) {
+            emails.send(users[i], userApps);
           }
         });
-
-        /*
-          $http({
-            method: 'POST',
-            url: /api/reminder,
-            data: userApps
-          });
-        */
       }
-    }
-  }, 1000 * 60);
+    });
+  }, 1000 * 60 /* * 60 * 24 */)
 };
+// var allApps = Apps.find({});
+//   console.log('WEEKLY REMINDER !!')
+//   console.log('allUsers helpers.js');
+//   console.log('allApps helpers.js', allApps);
+//   setInterval(function() {
+//     console.log('allUsers helpers.js', allUsers);
+//     console.log('allApps helpers.js', allApps);
+//     var day = new Date();
+//     day = day.getDay(); // return number 0-6 where 0 is sunday
+//     if(day === 1) {
+//       for(var i = 0; i < allUsers.length; i++) {
+//         var userApps = [];
+//         allApps.forEach(function(app) {
+//           if(app.userId === allUsers[i].id) {
+//             userApps.push(app);
+//           }
+//         });
+
+//         /*
+//           $http({
+//             method: 'POST',
+//             url: /api/reminder,
+//             data: userApps
+//           });
+//         */
+//       }
+//     }
+//   }, 1000 * 60);
+// };
 
 module.exports = weeklyReminderSender;
