4f490837803572da4410815c91a9d466ecffb479
Jerry Shanahan <JerryShanahan@Jerrys-MacBook-Pro.local>
2016-12-13 17:00:59 -0500

    refactor(es6 syntax):

--- server/email/emailModel.js
+++ server/email/emailModel.js
@@ -1,9 +1,9 @@
-var emailConfig = require('./emailConfig');
-var nodemailer = require('nodemailer'); // npm package for sending emails
-var Users = require('../users/userModel.js');
-var path = require('path');
+const emailConfig = require('./emailConfig');
+const nodemailer = require('nodemailer'); // npm package for sending emails
+const Users = require('../users/userModel.js');
+const path = require('path');
 
-var transporter = nodemailer.createTransport({ // create connection to emails through
+const transporter = nodemailer.createTransport({ // create connection to emails through
   host: 'smtp.gmail.com',
   secureConnection: true,
   auth: {
@@ -12,7 +12,7 @@ var transporter = nodemailer.createTransport({ // create connection to emails th
   }
 });
 
-var templates = { // each function will return a object filled with user varables that will represent the email template
+const templates = { // each function will return a object filled with user varables that will represent the email template
   newApp: (user ,appInfo) => ( // called in newSend fn below
     {
       from: '"AppTrak" <' + emailConfig.email_user + '>',
@@ -94,9 +94,9 @@ var templates = { // each function will return a object filled with user varable
 // };
 
 // all functions called by routes.js except send
-var email = {
+const email = {
   send: (user, userApps) => { // fn is called by weeklyReminder fn in emailHelpers.js
-    var options = templates.weeklyReminder(user.username, user.email, userApps.length); // set options to templates.weeklyReminder fn above. params = (username, emailAddress, numberofAppsOpen)
+    let options = templates.weeklyReminder(user.username, user.email, userApps.length); // set options to templates.weeklyReminder fn above. params = (username, emailAddress, numberofAppsOpen)
     transporter.sendMail(options, (error, info) => { // call transporter.sendMail with options and callback
       if(error) {
         return console.log('ERROR: ', error); // if error return and console log message
@@ -107,10 +107,10 @@ var email = {
     })
   },
   newSend: (req, res) => {
-    var user;
+    let user;
     Users.find({_id:req.body.data.userId}, (err, result) => { // search for user by userId passed in req
       user = result[0]; // set user to the user object that was returned from DB
-      var options = templates.newApp(user, req.body.data); // set options to the returned object from templates.newApp fn above.
+      let options = templates.newApp(user, req.body.data); // set options to the returned object from templates.newApp fn above.
       transporter.sendMail(options, (err, info) => { // calls transporter.sendMail built-in fn passing in options and callback
         if(err) { return console.log('ERROR: ', err); } // if error return and console.log error
         console.log('NEW APP Message Sent: ', info.response); // if message sent successfully console log message
@@ -121,11 +121,11 @@ var email = {
 
   },
   closedSend: (req, res) => {
-    var user;
+    let user;
     Users.find({_id:req.body.data.userId}, (err, results) => { // search for userId passed in req
       user = results[0]; // set user to the user object returned from DB
       if(user) { // if user not undefined
-        var options = templates.closedApp(user, req.body.data); // set options to object returned from templates.closedApp fn above
+        let options = templates.closedApp(user, req.body.data); // set options to object returned from templates.closedApp fn above
         transporter.sendMail(options, (err, info) => { // calls transporter.sendMail built-in fn passing in options and callback
           if(err) { return console.log('ERROR: ', err); }
           console.log('CLOSED APP Message Sent: ', info.response);
@@ -136,13 +136,13 @@ var email = {
     });
   },
   deletedSend: (req, res) => {
-    var app = req.body.data;
-    var user;
+    let app = req.body.data;
+    let user;
     Users.find({_id:req.body.data.userId}, (err, result) => {
       if(err) {console.log("ERROR: ", err)}
       user = result[0];
 
-      var options = templates.deletedApp(user, app); // set options to object returned from templates.deletedApps fn above
+      let options = templates.deletedApp(user, app); // set options to object returned from templates.deletedApps fn above
       transporter.sendMail(options, (err, info) => { // calls transporter.sendMail built-in fn passing in options and callback
         if(err) { return console.log('ERROR: ', err); } // if error return and console log error
         console.log('DELETED APP Message Sent: ', info.response); // if message sent successfully console log message
